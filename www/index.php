5988432a6906d198e951467ef7cc008c30d6aed5
MGranc <herrytr@mail.ru>
2022-12-20 14:36:53 +1000

    commit

--- www/index.php
+++ www/index.php
@@ -1,15 +1,13 @@
 <?php
-	//error_reporting(0);
+	error_reporting(0);
 	session_start();
 	include '../config/config.php';          //инициализация настроек
 	include '../library/mainFunctions.php';  // основные функции
 
 	//определяем с каким контроллером будем работать
 	$controllerName = isset($_GET['p']) ? ucfirst($_GET['p']) : 'Main';
-	echo $controllerName;
 	// определяем с какой функцией будем работать
 	$actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
-	echo $actionName;
 	//загружаем нужную страницу
 	loadPage($controllerName, $actionName);
 
